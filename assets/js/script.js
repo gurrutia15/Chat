324d2a8665a4d877d8f9fbbfe85d6f8ef22568f9
gurrutia15 <gurrutia15@gmail.com>
2023-05-23 01:02:05 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function sinNegrita() {
+    document.getElementById('nombreHoy').style.fontWeight = '600'
+}
+
+function sinNegritaTemprano() {
+    document.getElementById('nombreTemprano').style.fontWeight = '600'
+}
